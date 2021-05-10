f8a2dc8856928dd5690735ad5d2211f7d3289fa6
DINESH_KINI <dinesh.kini@oracle.com>
2021-05-11 00:51:16 +0530

    TestJava update

--- src/TestJava.java
+++ src/TestJava.java
@@ -2,8 +2,8 @@
 public class TestJava {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-
+		System.out.println("Hello Java");
+		System.out.println("Hello Guys!");
 	}
 
 }
